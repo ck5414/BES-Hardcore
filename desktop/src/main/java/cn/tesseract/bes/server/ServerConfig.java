d4941d5c43c2599d78240f19c9a571ec117b60ab
Marcel Mozer <marcel@mozer.pl>
2026-04-27 15:07:15 +0200

    Update server IP, MOTD, and configuration settings

--- desktop/src/main/java/cn/tesseract/bes/server/ServerConfig.java
+++ desktop/src/main/java/cn/tesseract/bes/server/ServerConfig.java
@@ -4,13 +4,13 @@ import java.util.ArrayList;
 import java.util.Random;
 
 public class ServerConfig {
-    public String ip = "127.0.0.1";
+    public String ip = "0.0.0.0";
     public int port = 25565;
     public long seed = new Random().nextLong();
-    public String motd = "打破一切！服务器官方群：1085649633";
+    public String motd = "A MITE Break Everything Server.";
     public ArrayList<String> ops = new ArrayList<>();
-    public boolean hardcore = false;
-    public boolean worldReset = false;
+    public boolean hardcore = true;
+    public boolean worldReset = true;
 
     public void normalize() {
         if (port < 1 || port > 65535) {
